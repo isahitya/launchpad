3c8c38b8a685050e8705670781b65f8b351ee269
Harshit Pandey <mailtoharshit@gmail.com>
2020-07-26 01:10:43 -0700

    Updated readme for the installation

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -30,7 +30,7 @@ export default {
   },
 
   methods: {
-    categorySelected(categoryId) {
+      setSelectedCategory(categoryId) {
       this.$store.dispatch("setSearchFilter", "");
       this.$store.dispatch("setSelectedCategory", categoryId);
       this.moveSlider(categoryId);
